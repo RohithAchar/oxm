0491cd223e6eba1ad2bd09870407f644a9617ad6
Rohith Achar <rohithachar2000@gmail.com>
2025-08-26 16:53:33 +0530

    removed msg91

--- app/(auth)/verify-phone/page.tsx
+++ app/(auth)/verify-phone/page.tsx
@@ -1,13 +1,8 @@
-import OTPForm from "@/components/otp-form";
-import OTPProvider from "@/components/otp-provider";
-
 
 export default function Home() {
   return (
     <main className="p-8">
-      <OTPProvider />
-      <h1 className="text-xl font-bold mb-4">MSG91 OTP Example</h1>
-      <OTPForm />
+      Hello there!
     </main>
   );
 }
